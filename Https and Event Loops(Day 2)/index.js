753999aaea048dbe9e400d502147c06385fbae2a
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-03-06 18:46:40 +0530

    Added day3

--- Https and Event Loops(Day 2)/index.js
+++ Https and Event Loops(Day 2)/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const books = require('./data')
+const books = require('./data');
 
 app.get("/home" ,function (request,response){
     response.send("Hello");
