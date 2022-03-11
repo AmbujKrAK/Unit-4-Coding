0cfe133457506fd50a008199d206d1cd5ced5a3e
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-03-11 20:28:05 +0530

    no chnage

--- Express- Relationships(Day 6)/Assignment/index.js
+++ Express- Relationships(Day 6)/Assignment/index.js
@@ -209,6 +209,7 @@ app.get("/books/:authorId/:sectionId", async (req, res) => {
 app.listen(5000, async () => {
     try {
         await connect()
+        console.log("Connection Established")
         console.log("Listening to port 5000");
     } catch (error) {
         console.log("Please check spelling");
