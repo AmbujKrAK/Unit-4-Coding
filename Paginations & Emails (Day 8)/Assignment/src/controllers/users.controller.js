b0dc74b2811617b3842a7408f1cb1c1448f7c507
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-03-26 22:22:37 +0530

    Starting project

---
+++ Paginations & Emails (Day 8)/Assignment/src/controllers/users.controller.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const User = require('../models/users.model');
+const app = express();
+
+app.get("", async(req,res)=>{
+    try {
+        const users = await User.find({}).lean().exec();
+        return res.status(200).send(users);
+    } catch (error) {
+        return res.status(500).send(error);
+    }
+});
+
+
