b8f21957a6422edcad242aa92e53f5cdf31271ae
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-03-28 11:00:46 +0530

    change

--- Unit 4 Evaluation 4/src/controllers/user.controller.js
+++ Unit 4 Evaluation 4/src/controllers/user.controller.js
@@ -2,5 +2,6 @@ const express = require("express");
 const crudController = require('./crud.controller');
 const User = require('../models/user.model');
 const app = express();
+const authenticate = require("../middleware/auth")
 
 app.post("/", crudController.post(User));
