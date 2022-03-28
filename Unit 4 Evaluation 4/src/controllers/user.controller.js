d2444f81f1e5d36316d02c5b9305159516280ce7
Ambuj Kumar <89453669+AmbujKrAK@users.noreply.github.com>
2022-03-28 10:56:44 +0530

    post

--- Unit 4 Evaluation 4/src/controllers/user.controller.js
+++ Unit 4 Evaluation 4/src/controllers/user.controller.js
@@ -1,3 +1,6 @@
 const express = require("express");
 const crudController = require('./crud.controller');
 const User = require('../models/user.model');
+const app = express();
+
+app.post("/", crudController.post(User));
